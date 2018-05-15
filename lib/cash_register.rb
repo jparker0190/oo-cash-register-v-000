0f740c7c3930c7e1ff0c1909667b36287d038114
Joseph Parker <jparker010190@gmail.com>
2018-05-15 00:54:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,6 +24,6 @@ class CashRegister
   end
   def void_last_transaction
     self.total -= self.last_transaction[1] * self.last_transaction[2]
-    self.last_transaction[2].times {self.items.delete_at(self.items.index(self.last_transaction[0]) self.items.count)}
+    self.last_transaction[2].times {self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)}
   end
 end
