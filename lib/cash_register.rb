21b7750ad0c484f933ebd2f977a4716430ce77ea
Joseph Parker <jparker010190@gmail.com>
2018-05-15 00:38:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,9 +9,6 @@ class CashRegister
     self.total = cost * number
     @items << name
   end
-  def add_item2(cost,name,number = 1)
-    self.total = cost * number
-  end
   def apply_discount
     if discount != 0
       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
